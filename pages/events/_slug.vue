52e3ef39805741abcf9e9738d366e7d89efebee9
123jpcoding <jcphelps02@gmail.com>
2022-09-09 12:17:46 +1000

    fetching events by year - not finished

--- pages/events/_slug.vue
+++ pages/events/_slug.vue
@@ -1,35 +1,44 @@
 <template>
 	<div class="container">
-		<h2>{{  $route.params.slug  }}</h2>
+		<h2>{{ $route.params.slug }}</h2>
 		<h3>Filename: pages/events/_slug.vue</h3>
 
 		<SiteNavigation />
 
 		<div class="event">
-			<!-- the buiding returns an array with one item in it, so need to reference it below -->
-			<h2>{{  events.title.rendered  }}</h2>
+			<h2>{{ events.title.rendered }}</h2>
+			<p v-if="$fetchState.pending">
+				<span class="loading"></span>
+			</p>
+			<p v-else-if="$fetchState.error">Error while fetching events</p>
 
-				<p>Summary: {{  events.content.rendered  }}</p>
-	
-			<!-- <pre>{{ $data }}</pre> -->
+			<section>
+				<img :src="event.image" :alt="event.title" />
+				<p>{{ event.content }}</p>
+			</section>
+			<button @click="goBack">Back</button>
+
 		</div>
 	</div>
 </template>
 
 <script>
-// in this API call, we go get a specifci building, filtered by the ID in the URL
-// we access what is in the URL by using the params object
+// in this API call, we go get specific events
 export default {
-	async asyncData({ params }) {
-		const events = await fetch(
-			`http://cm.beneb.com/wp-json/wp/v2/events/${params.slug}`
-		).then((res) => {
-			if (res.ok) {
-				return res.json()
-			}
-			throw new Error(res.status)
-		})
-		return { events }
+	data() {
+		return {
+			event: {}
+		}
 	},
+	async fetch() {
+		this.event = await this.$http.$get(
+			`http://cm.beneb.com/wp-json/wp/v2/events/?per_page=100/${this.$route.params.slug}`
+		)
+	},
+	methods: {
+		goBack() {
+			return this.$router.go(-1)
+		}
+	}
 }
 </script>
--- pages/events/index.vue
+++ pages/events/index.vue
@@ -7,34 +7,16 @@
 
 
 		<div class="events">
-			<!-- The building list is over in the components folder -->
-			<!-- the code below loads the component here so we can use it again -->
-			<EventsList />
+			<!-- Shows the Events Navigation -->
+			<PastEventsNav />
+			<h3>Upcoming Events</h3>
+			<UpcomingEvents />
 
 			<!-- this is a really useful line of code that will just output what the API call returns -->
 			<!-- <pre>{{ $data }}</pre> -->
 		</div>
 	</div>
 </template>
-
-<script>
-// in this API call, we go get a specific building, filtered by the ID in the URL
-// we access what is in the URL by using the params object
-export default {
-	async asyncData({ params }) {
-		const events = await fetch(
-			`http://cm.beneb.com/wp-json/wp/v2/events/`
-		).then((res) => {
-			if (res.ok) {
-				return res.json()
-			}
-			throw new Error(res.status)
-		})
-		return { events }
-	},
-}
-</script>
-
 
 <style>
 </style>
